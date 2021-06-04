52b220f2a5e1243b49d7688617ec86e4860d5d67
vx-charles <vx.charles@gmail.com>
2021-06-04 18:02:37 -0300

    Created the list of genre menus for the sidebar and created the list of movies for the content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
+import { useContext } from "react";
+import { Button } from "../components/Button";
+import "../styles/sidebar.scss";
+import { MoviesContext } from "../moviesContext";
+
 export function SideBar() {
-  // Complete aqui
+
+  const { selectedGenreId, setSelectedGenreId, genres } = useContext(MoviesContext)
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
